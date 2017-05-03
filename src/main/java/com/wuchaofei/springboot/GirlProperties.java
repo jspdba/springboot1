70ecca616ecd23648e5e348627690d3cecc27f2e
jspdba <jspdba@163.com>
2017-05-03 14:37:52 +0800

    first

---
+++ src/main/java/com/wuchaofei/springboot/GirlProperties.java
@@ -0,0 +1,9 @@
+package com.wuchaofei.springboot;
+
+/**
+ * Created by cofco on 2017/5/3.
+ */
+
+
+public class GirlProperties {
+}
---
+++ src/main/java/com/wuchaofei/springboot/HelloController.java
@@ -0,0 +1,17 @@
+package com.wuchaofei.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by cofco on 2017/5/3.
+ */
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
+    public String say(){
+        return "hi you!!";
+    }
+}
